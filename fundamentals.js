a692f3fac17f880d660a99416fd0f5088c59a7f4
Sanjay Persaud <sanjaywallah@gmail.com>
2017-07-18 13:56:26 -0400

    javaScript checkpoint

--- fundamentals.js
+++ fundamentals.js
@@ -3,29 +3,42 @@
 // #1: Create an array of strings called `colors` that contains three colors.
 // Type your solution immediately below this line:
 
-
+var colors = ['red', 'white', 'blue']
+console.log(colors)
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
-
+var last = colors[2]
+console.log(last)
 
 
 // #3: Create an empty array called `favoriteColors`.
 // Type your solution immediately below this line:
-
+var favoriteColors = []
+ console.log(favoriteColors)
 
 
 // #4: Create a `for` loop that adds each string in `colors` to `favoriteColors`.
 // Type your solution immediately below this line:
+for(i = 0; i < colors.length; i++) {
+  favoriteColors.push(colors[i])
+}
 
 
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
-
+var instructor = {
+  firstName: 'John',
+  lastName: 'Blee',
+  age: 60
+}
+console.log(instructor)
 
 
 // #6: Add a `facial-hair` (spelled exactly) property to `instructor` by accessing
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+instructor['facial-hair'] = true
+console.log( instructor['facial-hair'] )
--- hof.js
+++ hof.js
@@ -10,11 +10,14 @@ var people = [
 // person in the `people` array. Assign the returned array to a variable
 // called `peoplesAges`.
 // Type your solution immediately below this line:
-
-
-
+var peoplesAges = people.map(function (persons) {
+  return persons.age
+})
 
 // #2: Use the `filter` array method to create a new, filtered array containing only
 // persons from the `people` array who are older than 35. Assign the returned array
 // to a variable called `peopleOlderThan35`.
 // Type your solution immediately below this line:
+var peopleOlderThan35 = people.filter(function (olderPersons) {
+  return olderPersons.age > 35
+})
